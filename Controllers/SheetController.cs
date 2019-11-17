62f764ca7330378f1e74269fc6be4cd981b036c5
arriff <m-adda@hotmail.nl>
2019-11-17 12:59:24 +0100

    code reformat

--- Controllers/SheetController.cs
+++ Controllers/SheetController.cs
@@ -11,9 +11,24 @@ namespace QienUrenMachien.Controllers
 {
     public class SheetController : Controller
     {
-        public IActionResult TimeSheet()
+        public IActionResult Index()
         {
-            return View();
+            return View("Month");
+        }
+        public IActionResult TimeSheet(int Year, int Month)
+        {
+            var result = GetAllDaysInMonth(Year, Month);
+            return View(result);
+        }
+
+        public List<DateTime> GetAllDaysInMonth(int year, int month)
+        {
+            var ret = new List<DateTime>();
+            for (int i = 1; i <= DateTime.DaysInMonth(year, month); i++)
+            {
+                ret.Add(new DateTime(year, month, i));
+            }
+            return ret;
         }
     }
 }
