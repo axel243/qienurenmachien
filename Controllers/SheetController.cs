a19bbba20667689561b4eebd2ad263a9e9b2f151
arriff <m-adda@hotmail.nl>
2019-11-17 12:16:24 +0100

    timesheet

---
+++ Controllers/SheetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QienUrenMachien.Models;
+
+namespace QienUrenMachien.Controllers
+{
+    public class SheetController : Controller
+    {
+        public IActionResult TimeSheet()
+        {
+            return View();
+        }
+    }
+}
