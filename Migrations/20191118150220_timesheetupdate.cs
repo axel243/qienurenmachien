253d0fd8740bc383bb3614c3c6fda6a20fbbff85
Juless89 <j.m.r.kramer@gmail.com>
2019-11-18 16:05:58 +0100

    timesheet fix

---
+++ Migrations/20191118150220_timesheetupdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QienUrenMachien.Migrations
+{
+    public partial class timesheetupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TimeSheets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "TimeSheets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
