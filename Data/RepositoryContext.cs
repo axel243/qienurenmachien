549c245e0dd5db1029ab21343d463f38bc77aedb
Axel <Axel.Lauffer@hva.nl>
2019-11-18 11:18:19 +0100

    added migration to database + updated timesheet model

--- Data/RepositoryContext.cs
+++ Data/RepositoryContext.cs
@@ -6,7 +6,7 @@ namespace QienUrenMachien.Data
 {
     public class RepositoryContext : IdentityDbContext
     {
-        public TimeSheet timeSheet { get; set; }
+        public DbSet<TimeSheet> TimeSheets { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options)
     :       base(options)
         {
--- Models/TimeSheet.cs
+++ Models/TimeSheet.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QienUrenMachien.Models
 {
+    public enum Status { New, ForApproval, Approved, NotApproved }
     public class TimeSheet
     {
 
+        [Key]
         public int SheetID { get; set; }
         public string Project { get; set; }
         public string Month { get; set; }
@@ -17,8 +21,9 @@ namespace QienUrenMachien.Models
         public double Absence { get; set; }
         public double Training { get; set; }
         public double Other { get; set; }
+        public Status Status { get; set; }
         public string Data { get; set; }
 
-    
+
     }
 }
