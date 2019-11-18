d2c6133027bf7fb7728043eb189e7d26305d6017
TiesW <tieswijnker@gmail.com>
2019-11-18 12:42:58 +0100

    Admin Dashboard met user overview toegevoegd

--- Data/RepositoryContext.cs
+++ Data/RepositoryContext.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using QienUrenMachien.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace QienUrenMachien.Data
 {
     public class RepositoryContext : IdentityDbContext
     {
+        public RepositoryContext()
+        {
+
+        }
 
         public RepositoryContext(DbContextOptions<RepositoryContext> options)
     :       base(options)
