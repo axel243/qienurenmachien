e08dcaeaf9dab199f3054a94b7f3d41e4755c246
TiesW <tieswijnker@gmail.com>
2019-11-18 12:49:47 +0100

    Admin Dashboard met user overview toegevoegd

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QienUrenMachien.Data;
+
+namespace QienUrenMachien.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminDashboard()
+        {
+            var userlist = userManager.Users;
+            return View(userlist);
+        }
+
+        public IActionResult ViewUser(string Id)
+        {
+            var singleuser = userManager.Users.Single(u => u.Id == Id);
+            return View(singleuser);
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QienUrenMachien.Models
+{
+    public class UserModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Test { get; set; }
+    }
+}
