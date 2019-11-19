62784c6d2c45eccd50210f1a340f91f4e5e3c79d
Axel <Axel.Lauffer@hva.nl>
2019-11-19 16:30:58 +0100

    Days serialized to json and added to the data property

--- Controllers/SheetController.cs
+++ Controllers/SheetController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using QienUrenMachien.Data;
 using QienUrenMachien.Models;
 
@@ -18,48 +19,30 @@ namespace QienUrenMachien.Controllers
         {
             this.repo = repo;
         }
-        public IActionResult Index()
+        public IActionResult Month()
         {
 
-            var sheet1 = GetTimeSheets();
-            var sheet1DATA = sheet1[0].Data;
-            var json = Newtonsoft.Json.JsonConvert.DeserializeObject<TimeSheet>(sheet1DATA);
-            //            {
-            //                "1": { "SheetID": 3, "Project": "bla", "Month": "januari", "ProjectHours": 10, "Overwork": 15, "Sick": 20, "Absence": 25, "Training": 30, "Other": 35, "Status": 1 },
-            //                "2": { "SheetID": 3, "Project": "macaw", "Month": "januari", "ProjectHours": 11, "Overwork": 16, "Sick": 17, "Absence": 18, "Training": 19, "Other": 20, "Status": 1 }
-            //            }
-
-
-            return View("Month");
+            return View();
         }
         public IActionResult TimeSheet(int Year, int Month)
         {
 
             var test = GetTimeSheets();
             ViewBag.Test = test;
-
-
+            var jsonString = JsonConvert.SerializeObject(GetAllDaysInMonth(Year, Month));
+            TimeSheet timesheet = new TimeSheet();
+            timesheet.Data = jsonString;
             var result = GetAllDaysInMonth(Year, Month);
-            ViewBag.Result = result;
             return View(result);
         }
 
-        public String GetAllDaysInMonth(int year, int month)
+        public List<Day> GetAllDaysInMonth(int year, int month)
         {
             var days = new List<Day>();
-
-            Day daya = new Day(new DateTime(year, month, 1), "Nos", 4, 8, 2, 5, 7, 0, "None");
-            Day dayb = new Day(new DateTime(year, month, 2), "Gen25", 7, 8, 3, 9, 0, 0, "asasa");
-            days.Add(daya);
-            days.Add(dayb);
-            /*  for (int i = 1; i <= DateTime.DaysInMonth(year, month); i++)
-              {
-                  days.Add(new Day(new DateTime(year, month, i), "Nos", 4, 8, 2, 5, 7, 0, "None"));
-              }
-              return days;*/
-            var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(days);
-            return jsonString;
-
+            days.Add(new Day(new DateTime(year, month, 1), "Nos", 4, 4, 2, 5, 7, 0, "None"));
+            days.Add(new Day(new DateTime(year, month, 2), "Qien", 4, 4, 2, 5, 7, 0, "Eerder weggegaan"));
+            return days;
+            
         }
 
 
