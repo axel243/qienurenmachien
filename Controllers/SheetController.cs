38c13d185193d93a2e46c3569cb844e4b339f252
arriff <m-adda@hotmail.nl>
2019-11-20 09:09:05 +0100

    write to db

--- Controllers/SheetController.cs
+++ Controllers/SheetController.cs
@@ -21,11 +21,13 @@ namespace QienUrenMachien.Controllers
         public IActionResult Index()
         {
 
+
             return View("Month");
         }
         public IActionResult TimeSheet(int Year, int Month)
         {
-
+            ViewBag.Year = Year;
+            ViewBag.Month = Month;
             var test = GetTimeSheets();
             ViewBag.Test = test;
 
@@ -44,8 +46,54 @@ namespace QienUrenMachien.Controllers
             return ret;
         }
 
+        public ActionResult AddSheet()
+        {
+            List<DateTime> listDays = GetAllDaysInMonth(2019, 1);                               //maand en jaar is nog hardcoded
+            Dictionary<int, DaySheet> testobj = new Dictionary<int, DaySheet> { };
+            List<DaySheet> listDaySheets = new List<DaySheet>();
+
+            for (int i = 0; i < listDays.Count; i++)
+            {
+                listDaySheets.Add(new DaySheet { Day = i + 1, Month = listDays[i].ToString() });
+            }
 
 
+            for (int x = 0; x < listDaySheets.Count; x++)
+            {
+                testobj.Add(x + 1, listDaySheets[x]);
+            }
+
+            var jsonobj = Newtonsoft.Json.JsonConvert.SerializeObject(testobj);
+
+            return View(new TimeSheet { Data = jsonobj });
+        }
+
+        [HttpPost]
+        public ActionResult AddSheet(TimeSheet model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            AddNewSheet(model);                             // method hoort in de repository 
+            return RedirectToAction("Index");
+        }
+
+        public void AddNewSheet(TimeSheet timeSheetModel)
+        {
+            repo.TimeSheets.Add(new TimeSheet
+            {
+                Project = timeSheetModel.Project,
+                Month = timeSheetModel.Month,
+                ProjectHours = timeSheetModel.ProjectHours,
+                Overwork = timeSheetModel.Overwork,
+                Sick = timeSheetModel.Sick,
+                Absence = timeSheetModel.Absence,
+                Training = timeSheetModel.Training,
+                Other = timeSheetModel.Other,
+                Data = timeSheetModel.Data
+                
+            });
+            repo.SaveChanges();
+        }
         public List<TimeSheet> GetTimeSheets()
         {
             var sheetList = repo.TimeSheets.Select(n => new TimeSheet
