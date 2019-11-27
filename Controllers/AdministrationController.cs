674a8a2263cee877550ae6f92908dd5f3740108c
TiesW <tieswijnker@gmail.com>
2019-11-28 00:28:55 +0100

    Overzicht urentotaal per maand, en weekenddagen in timesheet met specifieke kleur

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -86,7 +86,7 @@ namespace QienUrenMachien.Controllers
 
         public async Task<IActionResult> TimeSheetOverview()
         {
-            TimeSheetsViewModel model = new TimeSheetsViewModel { Month = DateTime.Now.ToString("MMMM") };
+            TimeSheetsViewModel model = new TimeSheetsViewModel { Month = DateTime.Now.ToString("MMMM"), Year = DateTime.Now.Year };
             model.Employees = await repo.GetAllEmployeeTimeSheets(model);
             model.Trainees = await repo.GetAllTraineeTimeSheets(model);
             model.Months = repo.GetMonths();
