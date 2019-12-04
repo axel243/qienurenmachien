bfe9b22146afa9fc2109a8334b8a2a78c320eb45
Juless89 <j.m.r.kramer@gmail.com>
2019-12-04 09:39:46 +0100

    temp

---
+++ Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QienUrenMachien.Models;
+
+namespace QienUrenMachien.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        [HttpGet("")]
+        public IEnumerable<Note> List(string username)
+        {
+            return new List<Note>{
+                new Note{Title = "Shopping list", Contents="Some Apples"},
+                new Note{Title = "Thoughts on .NET Core", Contents="To follow..."}
+             };
+        }
+    }
+}
