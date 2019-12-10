35e37d83a1c1c8a8306bb9724a1960a9df56f93f
Mohamed a <m-adda@hotmail.nl>
2019-12-10 14:57:33 +0100

    multiple file upload

---
+++ Models/FileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QienUrenMachien.Models
+{
+    public class FileViewModel
+    {
+        public List<IFormFile> Files { get; set; }
+    }
+}
